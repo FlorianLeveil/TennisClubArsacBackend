a270f16c03b42397a932d3dea3019a095ccd7dd5
antoine <antoine.nicolleau@gmail.com>
2024-10-25 15:53:26 +0200

    [REFACTO PERMISSIONS]

--- BackendTennis/permissions/category_permissions.py
+++ BackendTennis/permissions/category_permissions.py
@@ -1,37 +1,6 @@
-from rest_framework import permissions
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.exceptions import AuthenticationFailed
+from BackendTennis.models import Category
+from BackendTennis.permissions.BasePermission import BasePermissions
 
 
-class CategoryPermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        jwt_auth = JWTAuthentication()
-        try:
-            user_auth_tuple = jwt_auth.authenticate(request)
-            if user_auth_tuple is not None:
-                request.user, _ = user_auth_tuple  # Unpack user and token data
-            else:
-                return False
-        except AuthenticationFailed:
-            return False
-
-        if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
-            if not request.user.is_authenticated:
-                return False
-
-            model_perms = {
-                'POST': 'add_category',
-                'PUT': 'change_category',
-                'PATCH': 'change_category',
-                'DELETE': 'delete_category',
-            }
-            perm = model_perms.get(request.method, None)
-            if perm and request.user.has_perm(f'BackendTennis.{perm}'):
-                return True
-            if request.user.is_superuser:
-                return True
-
-        return False
+class CategoryPermissions(BasePermissions):
+    model_name = Category.__name__.lower()
